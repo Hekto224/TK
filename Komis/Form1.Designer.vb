f4a42ab54876f26db33a3ccb5bd8ffffab2401c3
Hekto224 <154089747+Hekto224@users.noreply.github.com>
2023-12-17 20:28:19 +0100

    Add files via upload

---
+++ Komis/Form1.Designer.vb
@@ -0,0 +1,225 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.ListBox2 = New System.Windows.Forms.ListBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.ListBox3 = New System.Windows.Forms.ListBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.ListBox4 = New System.Windows.Forms.ListBox()
+        Me.ListBox5 = New System.Windows.Forms.ListBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.ListBox6 = New System.Windows.Forms.ListBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Items.AddRange(New Object() {"Mercedes", "BMW", "Mazda", "Skoda"})
+        Me.ListBox1.Location = New System.Drawing.Point(26, 38)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(120, 95)
+        Me.ListBox1.TabIndex = 0
+        '
+        'ListBox2
+        '
+        Me.ListBox2.FormattingEnabled = True
+        Me.ListBox2.Location = New System.Drawing.Point(188, 38)
+        Me.ListBox2.Name = "ListBox2"
+        Me.ListBox2.Size = New System.Drawing.Size(120, 95)
+        Me.ListBox2.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(26, 19)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(37, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Marka"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(188, 19)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(36, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Model"
+        '
+        'ListBox3
+        '
+        Me.ListBox3.FormattingEnabled = True
+        Me.ListBox3.Location = New System.Drawing.Point(346, 38)
+        Me.ListBox3.Name = "ListBox3"
+        Me.ListBox3.Size = New System.Drawing.Size(120, 95)
+        Me.ListBox3.TabIndex = 4
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(346, 19)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(81, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Dostępne silniki"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(472, 170)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(316, 229)
+        Me.PictureBox1.TabIndex = 6
+        Me.PictureBox1.TabStop = False
+        '
+        'ListBox4
+        '
+        Me.ListBox4.FormattingEnabled = True
+        Me.ListBox4.Location = New System.Drawing.Point(26, 170)
+        Me.ListBox4.Name = "ListBox4"
+        Me.ListBox4.Size = New System.Drawing.Size(120, 95)
+        Me.ListBox4.TabIndex = 7
+        '
+        'ListBox5
+        '
+        Me.ListBox5.FormattingEnabled = True
+        Me.ListBox5.Location = New System.Drawing.Point(188, 170)
+        Me.ListBox5.Name = "ListBox5"
+        Me.ListBox5.Size = New System.Drawing.Size(120, 95)
+        Me.ListBox5.TabIndex = 8
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(29, 151)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(59, 13)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "Typ lakieru"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(191, 151)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(31, 13)
+        Me.Label5.TabIndex = 10
+        Me.Label5.Text = "Kolor"
+        '
+        'ListBox6
+        '
+        Me.ListBox6.FormattingEnabled = True
+        Me.ListBox6.Location = New System.Drawing.Point(346, 170)
+        Me.ListBox6.Name = "ListBox6"
+        Me.ListBox6.Size = New System.Drawing.Size(120, 95)
+        Me.ListBox6.TabIndex = 11
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(346, 150)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(126, 13)
+        Me.Label6.TabIndex = 12
+        Me.Label6.Text = "Dodatkowe wyposazenie"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(346, 314)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(100, 23)
+        Me.Button1.TabIndex = 13
+        Me.Button1.Text = "Dodaj pozycje"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(346, 288)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 14
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(349, 344)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(97, 23)
+        Me.Button2.TabIndex = 15
+        Me.Button2.Text = "Edytuj"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.ListBox6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.ListBox5)
+        Me.Controls.Add(Me.ListBox4)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.ListBox3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ListBox2)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Name = "Form1"
+        Me.Text = "Komis samochodowy"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents ListBox2 As ListBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents ListBox3 As ListBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents ListBox4 As ListBox
+    Friend WithEvents ListBox5 As ListBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents ListBox6 As ListBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button2 As Button
+End Class
---
+++ Komis/Form1.vb
@@ -0,0 +1,109 @@
+﻿Imports System.Net.Security
+
+Public Class Form1
+    Private Sub ListBox2_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+        If ListBox1.SelectedIndex = 0 Then
+            ListBox2.Items.Clear()
+            ListBox2.Items.Add("Klasa A")
+            ListBox2.Items.Add("Klasa C")
+        ElseIf ListBox1.SelectedIndex = 1 Then
+            ListBox2.Items.Clear()
+            ListBox2.Items.Add("3")
+            ListBox2.Items.Add("5")
+        ElseIf ListBox1.SelectedIndex = 2 Then
+            ListBox2.Items.Clear()
+            ListBox2.Items.Add("RX7")
+            ListBox2.Items.Add("RX8")
+            ListBox2.Items.Add("6")
+        ElseIf ListBox1.SelectedIndex = 3 Then
+            ListBox2.Items.Clear()
+            ListBox2.Items.Add("Octavia")
+        End If
+
+    End Sub
+
+    Private Sub ListBox3_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged, ListBox2.SelectedIndexChanged
+        If ListBox1.SelectedIndex = 0 And ListBox2.SelectedIndex = 0 Then
+            ListBox3.Items.Clear()
+            ListBox3.Items.Add("1.4 Benzyna")
+            ListBox3.Items.Add("2.0 Diesel")
+        ElseIf ListBox1.SelectedIndex = 0 And ListBox2.SelectedIndex = 1 Then
+            ListBox3.Items.Clear()
+            ListBox3.Items.Add("1.5 Benzyna")
+            ListBox3.Items.Add("2.0 Diesel")
+        ElseIf ListBox1.SelectedIndex = 1 And ListBox2.SelectedIndex = 0 Then
+            ListBox3.Items.Clear()
+            ListBox3.Items.Add("1.8 Benzyna")
+            ListBox3.Items.Add("2.0 Benzyna")
+        ElseIf ListBox1.SelectedIndex = 1 And ListBox2.SelectedIndex = 1 Then
+            ListBox3.Items.Clear()
+            ListBox3.Items.Add("2.0 Benzyna")
+        ElseIf ListBox1.SelectedIndex = 2 And ListBox2.SelectedIndex = 0 Then
+            ListBox3.Items.Clear()
+            ListBox3.Items.Add("1.3 Benzyna")
+        ElseIf ListBox1.SelectedIndex = 2 And ListBox2.SelectedIndex = 1 Then
+            ListBox3.Items.Clear()
+            ListBox3.Items.Add("1.3 Benzyna")
+        ElseIf ListBox1.SelectedIndex = 2 And ListBox2.SelectedIndex = 2 Then
+            ListBox3.Items.Clear()
+            ListBox3.Items.Add("2.0 Diesel")
+            ListBox3.Items.Add("2.5 Diesel")
+        ElseIf ListBox1.SelectedIndex = 3 And ListBox2.SelectedIndex = 0 Then
+            ListBox3.Items.Clear()
+            ListBox3.Items.Add("1.0 Benzyna")
+            ListBox3.Items.Add("1.5 Benzyna")
+            ListBox3.Items.Add("2.0 Diesel")
+        End If
+    End Sub
+
+    Private Sub ListBox4_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox3.SelectedIndexChanged
+        ListBox4.Items.Clear()
+        ListBox4.Items.Add("Metalic")
+        ListBox4.Items.Add("Mat")
+        ListBox4.Items.Add("Chrome")
+    End Sub
+
+    Private Sub ListBox5_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox4.SelectedIndexChanged
+        If ListBox4.SelectedIndex = 0 Then
+            ListBox5.Items.Clear()
+            ListBox5.Items.Add("Czerwony")
+            ListBox5.Items.Add("Bialy")
+            ListBox5.Items.Add("Czarny")
+        Else
+            ListBox5.Items.Clear()
+            ListBox5.Items.Add("Czerwony")
+            ListBox5.Items.Add("Bialy")
+            ListBox5.Items.Add("Czarny")
+            ListBox5.Items.Add("Niebieski")
+            ListBox5.Items.Add("Zielony")
+        End If
+    End Sub
+
+    Private Sub ListBox6_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox5.SelectedIndexChanged
+        ListBox6.Items.Clear()
+        ListBox6.Items.Add("Tempomat")
+        ListBox6.Items.Add("System glosniomowiacy")
+        ListBox6.Items.Add("Podgrzewany fotel")
+    End Sub
+
+    Private Sub PictureBox1_Click_1(sender As Object, e As EventArgs) Handles ListBox6.SelectedIndexChanged
+        'PictureBox1.Image = Image.FromFile("D:\Pawel\TK\Mercedes klasa A.jpg")
+        If ListBox1.SelectedIndex = 0 And ListBox2.SelectedIndex = 0 Then
+            PictureBox1.Image = Image.FromFile(System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Images", "Mercedes klasa A.jpg"))
+        ElseIf ListBox1.SelectedIndex = 0 And ListBox2.SelectedIndex = 1 Then
+            PictureBox1.Image = Image.FromFile(System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Images", "Mercedes klasa C.jpg"))
+        ElseIf ListBox1.SelectedIndex = 1 And ListBox2.SelectedIndex = 0 Then
+            PictureBox1.Image = Image.FromFile(System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Images", "BMW 3.jpg"))
+        ElseIf ListBox1.SelectedIndex = 1 And ListBox2.SelectedIndex = 1 Then
+            PictureBox1.Image = Image.FromFile(System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Images", "BMW 5.jpg"))
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ListBox6.Items.Add(TextBox1.Text)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        ListBox6.Items(ListBox6.SelectedIndex) = TextBox1.Text
+    End Sub
+End Class
